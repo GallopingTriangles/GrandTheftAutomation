184cbd999efae2f4f7effe3053c6863c12e90ae3
Chris Chiang <cchiang1992@gmail.com>
2016-09-22 19:15:32 -0700

    basic express server up at port 8888

---
+++ src/server/server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var path = require('path');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(8888);
+console.log('Listening on 127.0.0.1: 8000');
