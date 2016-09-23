c9b25d67b45ec673c239c9cfedb781fcc828b8b1
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-09-23 14:57:04 -0700

    (feat) added environment port variable

--- src/server/server.js
+++ src/server/server.js
@@ -1,13 +1,20 @@
+// == set up ===================================================
 var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
+var port = process.env.PORT || 8080;
 var path = require('path');
 var session = require('express-session');
 
 var app = express();
 
+// == configuration ============================================
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(8888);
-console.log('Listening on 127.0.0.1:8888');
+// == routes ===================================================
+
+// == listen ===================================================
+app.listen(port, () => {
+  console.log('listening on port', port);
+});
