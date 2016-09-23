b95901440ae2ab99ab2177a4fdb71836d648ce5e
Chris Chiang <cchiang1992@gmail.com>
2016-09-22 19:39:45 -0700

    basic redux store set up with no functionality on reducer, and no state

--- src/client/index.js
+++ src/client/index.js
@@ -2,3 +2,16 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import App from './app/components/App.jsx';
+import reducer from './app/reducers/reducer.js';
+
+var store = createStore(reducer);
+
+var render = () => {
+  ReactDOM.render(
+    <App />,
+    document.getElementById('app');
+  )
+}
+
+render();
+store.subscribe(render);
