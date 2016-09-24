c31f0b7d4a36186b86d84732bb44e41dc3a52667
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-09-23 19:08:51 -0700

    (feat) created folder structure

---
+++ src/server/routes/routes.js
@@ -0,0 +1,7 @@
+var router = require('express').Router();
+var testController = require('../controllers/testController');
+
+router.route('/')
+  .get(testController.get);
+
+module.exports = router;
