4b50e85563de0c38710e6bf0b8ef07c256b3c045
Chris Chiang <cchiang1992@gmail.com>
2016-09-23 21:28:05 -0700

    wrote some extra tests, altered App trying to map store to props but not working

--- src/client/app/components/App.jsx
+++ src/client/app/components/App.jsx
@@ -8,14 +8,34 @@ class App extends Component {
     super(props);
   }
   render() {
+    console.log(this.props);
     return (
       <div>
         <Nav />
         <p>Hey mang</p>
         <p>Grand Theft Automation</p>
+        <button onClick={ () => this.props.click() }>click</button>
+        <p>Here{ this.props.command }</p>
       </div>
     )
   }
 }
 
-export default connect()(App);
+var mapStateToProps = state => {
+  return {
+    command: state.command
+  }
+};
+
+var mapDispatchToProps = dispatch => {
+  return {
+    click: () => {
+      dispatch({
+        type: 'INPUT_COMMAND',
+        command: 'clicked!'
+      });
+    }
+  }
+}
+// mapping store to prop currently not working
+export default connect(mapStateToProps, mapDispatchToProps)(App);
