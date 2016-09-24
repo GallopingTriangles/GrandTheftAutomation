524456d47431551aa16435f0fecb445265c928ad
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-09-23 19:08:04 -0700

    (feat) log every request to console with morgan

--- src/server/server.js
+++ src/server/server.js
@@ -5,16 +5,20 @@ var morgan = require('morgan');
 var port = process.env.PORT || 8080;
 var path = require('path');
 var session = require('express-session');
+var routes = require('./routes/routes');
 
 var app = express();
 
 // == configuration ============================================
+app.use(express.static('../client')); 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev')); // log every request to the console
 
 // == routes ===================================================
+app.use('/api', routes);
 
 // == listen ===================================================
 app.listen(port, () => {
-  console.log('listening on port', port);
+  console.log('app listening on port', port);
 });
