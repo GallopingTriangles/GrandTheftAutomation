2d938da3e0ea6f25421084697525fd49695e2db9
Chris Chiang <cchiang1992@gmail.com>
2016-09-23 21:28:05 -0700

    wrote some extra tests, altered App trying to map store to props but not working

--- src/client/app/reducers/reducer.js
+++ src/client/app/reducers/reducer.js
@@ -1,8 +1,6 @@
-const reducer = (state = {}, action) => {
-  switch (action.type) {
-    default: 
-      return state;
-  }
-}
+import { combineReducers } from 'redux';
+import userCommand from './userCommand';
 
-export default reducer;
+export default combineReducers({
+  userCommand
+});
