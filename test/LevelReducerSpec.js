a56ca4a16cf36f18263abe9925eee53c91b6c026
Chris Chiang <cchiang1992@gmail.com>
2016-09-25 16:19:00 -0700

    added more unit tests for reducer with action creator for levels

--- test/LevelReducerSpec.js
+++ test/LevelReducerSpec.js
@@ -1,5 +1,6 @@
 import { expect } from 'chai';
 import level from '../src/client/app/reducers/Level';
+import changeLevel from '../src/client/app/actions/changeLevel';
 
 describe('Level change reducer', () => {
 
@@ -45,5 +46,24 @@ describe('Level change reducer', () => {
     result = level(result, action_2);
     expect(result).to.equal(2);
   });
+
+  describe('Level change reducer and changeLevel action creator', () => {
+
+    it('should change level appropriately with the created action', () => {
+      var action = changeLevel(1);
+      var result = level(undefined, action);
+      expect(result).to.equal(1);
+    });
+
+    it('should change levels appropriately with multiple actions', () => {
+      var result;
+      var action_1 = changeLevel(1);
+      var action_2 = changeLevel(2);
+      result = level(result, action_1);
+      expect(result).to.equal(1);
+      result = level(result, action_2);
+      expect(result).to.equal(2);
+    })
+  })
 
 })
