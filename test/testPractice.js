b1dc05bb569b37ed2585b52540217f95f6329f59
Kevin Chan <kevindchan@gmail.com>
2016-09-26 11:06:38 -0700

    Solves merge conflict in testPractice.js

--- test/testPractice.js
+++ test/testPractice.js
@@ -1,10 +1,14 @@
-import { assert } from 'chai';
+import { assert, expect } from 'chai';
+
+var assert = chai.assert;
+var expect = chai.expect;
+
 
 describe('Array', function() {
 
   it('should start empty', function() {
     var arr = [];
-    assert.equal(arr.length, 0);
+    assert.equal(arr.length, 0, 'Array length was not 0');
   });
   
 });
