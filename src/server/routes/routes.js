55c9700fb903f1c59bc572e4839563fa86d823af
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-09-26 10:56:31 -0700

    (feat) created api/user route

--- src/server/routes/routes.js
+++ src/server/routes/routes.js
@@ -1,8 +1,13 @@
-// EXAMPLE
+// == ROUTES ===================================================
 var router = require('express').Router();
-var testController = require('../controllers/testController');
 
-router.route('/')
-  .get(testController.get);
+var testController = require('../controllers/testController');
+var user = require('./user');
+
+// == test =====================================================
+router.route('/test').get(testController.get);
+
+// == user routes ==============================================
+router.route('/user', user);
 
 module.exports = router;
