8caa7370b16c2294f0e25ee920c804caba78ab3c
Kevin Chan <kevindchan@gmail.com>
2016-09-26 15:23:16 -0700

    Creates MySQL db and connection via /db/index.js

---
+++ src/server/db/index.js
@@ -0,0 +1,20 @@
+var mysql = require('mysql');
+
+// == CREATES MYSQL DATABASE ==============================================
+var connection = mysql.createConnection({
+  user: 'admin',
+  password: 'wasted',
+  database: 'gta'
+});
+
+// == ESTABLISHES A CONNECTION W/ MYSQL DATABASE ==========================
+connection.connect(function(err) {
+  if (err) {
+    console.log('error connecting to MySQL: ', err);
+  } else {
+    console.log('Connected as ID: ', connection.threadId);
+  }
+});
+
+module.exports = connection;
+
