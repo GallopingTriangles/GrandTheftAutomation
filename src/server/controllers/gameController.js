07a6e00d86279f68b88a92d8ab30d97bb92d12f4
Chris Chiang <cchiang1992@gmail.com>
2016-09-28 16:25:10 -0700

    handler for updating a log for a certain level for a certain user is complete

--- src/server/controllers/gameController.js
+++ src/server/controllers/gameController.js
@@ -15,7 +15,7 @@ module.exports = {
     /* req should contain the level and user and commands */
     var username = req.body.username;
     var level = req.body.level;
-    var log = req.body.log;
+    var solution = req.body.log;
     db.User.findOne({ where: { username: username }}).then(user => {
       if (!user) { 
         // handle error, but user should NOT be null since they must be logged in to save a solution
@@ -23,16 +23,29 @@ module.exports = {
         res.status(404).json('Processing error. Try again.');
       } else {
         var userId = user.dataValues.id;
-        db.Log.create({
-          level: level,
-          solution: log,
-          UserId: userId
+        db.Log.find({
+          where: {
+            level: level,
+            UserId: userId
+          }
         }).then(log => {
-          res.status(200).json('User log successfully saved.');
+          if (!log) { // if a log doesn't exist for the user at that level, create it
+            db.Log.create({
+              level: level,
+              solution: solution,
+              UserId: userId
+            }).then(log => {
+              res.status(200).json('User log successfully saved.');
+            }).catch(err => {
+              console.log('Error saving solution: ', err);
+              res.status(404).json('Processing error. Try again.');
+            });
+          } else { // if it does exist, then update it
+            module.exports.updateGameState(req, res, next);
+          }
         }).catch(err => {
-          console.log('Error saving solution: ', err);
-          res.status(404).json('Processing error. Try again.');
-        });
+          console.log('error searching: ', err);
+        })
       }
     }).catch(err => {
       res.status(404).json('Processing error. Try again.');
@@ -40,10 +53,49 @@ module.exports = {
   },
 
   updateGameState: (req, res, next) => {
-    /* handles a PUT request */
+    /* handles a PUT request with a 204 status on success         */
     /* updates the saved commands for user for a particular level
                to a set of new commands that the user has entered */
-    console.log('Received PUT request to update game state');
-    res.json('Received PUT request to update game state'); // status code 204 for success
+    var username = req.body.username;
+    var level = req.body.level;
+    var solution = req.body.log;
+    db.User.findOne({ where: { username: username }}).then(user => {
+      if (!user) {
+        // this error should never occur because a user should be logged in
+        res.status(404).json('Processing error. Try again');
+      } else {
+        var userId = user.dataValues.id;
+        // find the log corresponding to the user
+        db.Log.findOne({
+          where: {
+            level: level,
+            UserId: userId
+          }
+        }).then(log => {
+          // then update it
+          if (log) {
+            log.update({
+              solution: solution
+            }).then(() => {
+              console.log('Updated log: ', log.dataValues);
+              res.status(204).json('Updated solution');
+            }).catch(err => {
+              console.log('Error updating log: ', err);
+              res.status(404).json('Error updating solution');
+            })
+          } else {
+            // log didn't exist
+            console.log('Log did not exist.. weird');
+            res.status(404).json('Error updating solution');
+          }
+        }).catch(err => {
+          console.log('Error updating log: ', err);
+          res.status(404).json('Error updating solution');
+        })
+      }
+    }).catch(err => {
+      console.log('Error looking for user: ', err);
+      res.status(404).json('Processing error. Try again.');
+    })
   }
 }
