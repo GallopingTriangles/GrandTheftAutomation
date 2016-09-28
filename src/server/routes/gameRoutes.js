b51290bd6bacb89a87e6aef69d6a291b3627b076
Chris Chiang <cchiang1992@gmail.com>
2016-09-28 16:29:58 -0700

    removed PUT request handler because its unneeded

--- src/server/routes/gameRoutes.js
+++ src/server/routes/gameRoutes.js
@@ -5,6 +5,11 @@ router.get('/', gameController.getGameState); // gets the user's saved game stat
 
 router.post('/', gameController.saveGameState); // saves the user's game state of the code editor
 
-router.put('/', gameController.updateGameState); // updates the user's game state of the code editor
+// router.put('/', gameController.updateGameState); // updates the user's game state of the code editor
+/****************************************************************************
+// Don't need a handler for PUT requests
+// Send everything through POST request and it will automatically update the
+// log in the database if it already exists
+*****************************************************************************/
 
 module.exports = router;
