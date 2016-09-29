ce6afcff1a51aaf288c4ed6e71d10ed3c8ce014f
Kevin Chan <kevindchan@gmail.com>
2016-09-28 18:58:00 -0700

    Refactors router to use .route()

--- src/server/routes/userRoutes.js
+++ src/server/routes/userRoutes.js
@@ -1,12 +1,17 @@
 var router = require('express').Router();
 var userController = require('../controllers/userController.js');
 
-router.get('/', userController.verify, userController.getState); // returns user's saved stuff after verification
+router.route('/')
+  .all(userController.verify)
+  .get(userController.getState); // returns user's saved stuff after verification
 
-router.post('/login', userController.login); // authenticates the user when loggin in
+router.route('/login')
+  .post(userController.login); // authenticates the user when loggin in
 
-router.post('/signup', userController.signup); // checks and creates a new user to the database
+router.route('/signup')
+  .post(userController.signup); // checks and creates a new user to the database
 
-router.get('/logout', userController.logout); // wipes the session and logs the user out
+router.route('/logout')
+  .get(userController.logout); // wipes the session and logs the user out
 
 module.exports = router;
