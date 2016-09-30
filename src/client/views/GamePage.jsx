dba6cd7d587e50982a2255b18d59e04b6581e985
Chris Chiang <cchiang1992@gmail.com>
2016-09-30 16:48:06 -0700

    Add comments to GamePage: renders game container and code editor

--- src/client/views/GamePage.jsx
+++ src/client/views/GamePage.jsx
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import Game from '../app/containers/GameContainer.jsx';
 import Console from '../app/containers/ConsoleContainer.jsx';
 
+/*******************************************************/
+/* Renders the game page that consists of:             */
+/* (1) The game container                              */
+/* (2) The console (the code editor)                   */
+/*******************************************************/
+
 const GamePage = props => (
   <div>
     <Game />
