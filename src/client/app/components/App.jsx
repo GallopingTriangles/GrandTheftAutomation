ee01040ece61316ed3cfefc140f2ac2967687896
Chris Chiang <cchiang1992@gmail.com>
2016-09-30 16:18:59 -0700

    Implement react-router to navigate between landing page and game
    WARNING: CODE IS DRIPPING WET. NEED TO MODULARIZE.
    THIS IS ONLY AN INITIAL SETUP.

--- src/client/app/components/App.jsx
+++ src/client/app/components/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { Router, Route, IndexRoute, Link, hashHistory } from 'react-router';
 
 import Nav from './Nav.jsx';
 import Signup from '../containers/SignupContainer.jsx';
@@ -7,19 +8,52 @@ import Login from '../containers/LoginContainer.jsx';
 import Game from '../containers/GameContainer.jsx';
 import Console from '../containers/ConsoleContainer.jsx';
 
+const Container = props => (
+  <div>
+    <Nav />
+    { props.children }
+  </div>
+)
+
+const LandingPage = props => (
+  <div>
+    <Signup />
+    <Login />
+  </div>
+)
+
+const GamePage = props => (
+  <div>
+    <Game />
+    <Console />
+  </div>
+)
+
+const AllRoutes = props => (
+  <Router history={ hashHistory }>
+    <Route path='/' component={ Container }>
+      <IndexRoute component={ LandingPage } />
+      <Route path='game' component={ GamePage } />
+    </Route>
+  </Router>
+)
+
 class App extends Component {
   constructor(props) {
     super(props);
   }
   render() {
+    // return (
+    //   <div>
+    //     <Nav />
+    //     <Signup />
+    //     <Login />
+    //     <Game />
+    //     <Console />
+    //   </div>
+    // )
     return (
-      <div>
-        <Nav />
-        <Signup />
-        <Login />
-        <Game />
-        <Console />
-      </div>
+      <AllRoutes />
     )
   }
 }
--- src/client/router.js
+++ src/client/router.js
@@ -1,2 +1,17 @@
-import { Router, Route, Link, browserHistory } from 'react-router';
+import React, { Component } from 'react';
+import { Router, Route, Link, browserHistory, hashHistory } from 'react-router';
 
+class AllRoutes extends Component {
+  contructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Router history={ hashHistory } >
+        
+      </Router>
+    )
+  }
+}
