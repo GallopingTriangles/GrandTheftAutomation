9e393f5b6f476a014caa418f2789c5c7e52d08a4
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-09-30 20:34:24 -0700

    (feat) added functions to buttons

--- src/client/app/containers/FooterContainer.jsx
+++ src/client/app/containers/FooterContainer.jsx
@@ -1,19 +1,52 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import createCommand from '../actions/UserCommandAction.js';
+import changeLevel from '../actions/changeLevel.js';
 
 class Footer extends Component {
+  // == REACT FUNCTIONS =====================================================
   constructor(props) {
     super(props);
+    this.state = {
+      level: this.props.level
+    };
   }
 
+  // == CUSTOM FUNCTIONS ====================================================
+  previousLevel(e) {
+    e.preventDefault();
+    if (this.state.level > 1) {
+      changeLevel(this.state.level - 1);
+    }
+  }
+
+  nextLevel(e) {                                                              // go to next level
+    e.preventDefault();
+    if (this.state.level < 4) {
+      console.log('hey');
+      changeLevel(2);
+    }
+  }
+
+  // == RENDER FOOTER =======================================================
   render() {
-    console.log(this.state);
+    console.log('props',this.props)
     return (
       <footer className='footer'>
         <div className='container'>
-          <button className='btn btn-primary col-md-1' disabled>Back</button>
-          <h5 className='col-md-10 text-center'>1 / 4</h5>
-          <button className='btn btn-primary col-md-1'>Next</button>
+          <button 
+            className='btn btn-primary col-xs-1'
+            onClick={this.previousLevel.bind(this)} 
+          >
+          Back
+          </button>
+          <h5 className='col-xs-10 text-center'>{this.state.level} / 4</h5>
+          <button 
+            className='btn btn-primary col-xs-1'
+            onClick={this.nextLevel.bind(this)}
+          >
+          Next
+          </button>
         </div>
       </footer>  
     );
@@ -26,5 +59,21 @@ var mapStateToProps = state => {
   }
 }
 
-// connect() provides a link between the store and the component through the props
-export default connect(mapStateToProps)(Footer);
+var mapDispatchToProps = dispatch => {
+  return {
+    // previousLevel: (level) => {
+    //   // dispatch(changeLevel(level));
+    // },
+    // nextLevel: (level) => {
+    //   // dispatch(changeLevel(level));
+    // }
+    // sendCommand: (level, command) => {
+    //   dispatch(createCommand(level, command));
+    // }
+    changeLevel: (level) => {
+      dispatch(changeLevel(level));
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Footer);
