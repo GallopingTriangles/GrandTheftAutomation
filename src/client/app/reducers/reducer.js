7bf86a5f9f9a80444ab4c50323037d36c1c1424e
Chris Chiang <cchiang1992@gmail.com>
2016-10-01 10:57:08 -0700

    Add user reducer to root reducer; store now contains a user property for the currently logged in user

--- src/client/app/reducers/reducer.js
+++ src/client/app/reducers/reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import userCommand from './userCommand';
 import level from './level';
+import user from './user';
 
 export default combineReducers({
   userCommand,
-  level
+  level,
+  user
 });
