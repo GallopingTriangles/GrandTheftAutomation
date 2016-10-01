c44c47230981cf751439b743f3fcc01a625ccfd0
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-10-01 11:40:58 -0700

    (feat) changing state after button press to determine component that needs to be rendered

--- src/client/app/containers/ConsoleContainer.jsx
+++ src/client/app/containers/ConsoleContainer.jsx
@@ -12,23 +12,38 @@ class Console extends Component {
     };
   }
 
-  renderLearn() {
-    
+  // == RENDER FUNCTIONS =================================================================
+  renderLearn(e) {
+    e.preventDefault();
+    this.setState({tab: 'learn'});
   }
 
-  renderEditor() {
-    return <CommandLine />;
+  renderInstructions(e) {
+    e.preventDefault();
+    this.setState({tab: 'instructions'});
   }
 
+  renderEditor(e) {
+    e.preventDefault();
+    this.setState({tab: 'editor'});
+  }
+
+  renderBugs(e) {
+    e.preventDefault();
+    this.setState({tab: 'bugs'})
+  }
+
+  // == RENDER COMPONENTS ================================================================
   render() {
+    console.log('staat', this.state);
     return (
       <div className='console col-md-5'>
         <div className='console-header'>
           <div className='btn-group' role='group'>
-            <button type='button' className='btn btn-default' onClick={this.renderLearn}><i className='fa fa-book' aria-hidden='true'></i> Learn</button>
-            <button type='button' className='btn btn-default'><i className='fa fa-check-square-o' aria-hidden='true'></i> Instruction</button>
-            <button type='button' className='btn btn-default'><i className='fa fa-code' aria-hidden='true'></i> Code</button>
-            <button type='button' className='btn btn-default'><i className='fa fa-bug' aria-hidden='true'></i> Bug Report</button>
+            <button type='button' className='btn btn-default' onClick={this.renderLearn.bind(this)}><i className='fa fa-book' aria-hidden='true'></i> Learn</button>
+            <button type='button' className='btn btn-default' onClick={this.renderInstructions.bind(this)}><i className='fa fa-check-square-o' aria-hidden='true'></i> Instruction</button>
+            <button type='button' className='btn btn-default' onClick={this.renderEditor.bind(this)}><i className='fa fa-code' aria-hidden='true'></i> Code</button>
+            <button type='button' className='btn btn-default' onClick={this.renderBugs.bind(this)}><i className='fa fa-bug' aria-hidden='true'></i> Bug Report</button>
           </div>
         </div>
         <div className='console-content'>
