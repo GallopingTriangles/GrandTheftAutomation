745c94aa176f8b3938672f4e5fe4ae040a1d4e48
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-10-01 11:31:35 -0700

    (feat) created new console div with head outside of commandline

--- src/client/app/containers/ConsoleContainer.jsx
+++ src/client/app/containers/ConsoleContainer.jsx
@@ -7,16 +7,48 @@ import LogsContainer from './LogsContainer.jsx';
 class Console extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      tab: 'editor'
+    };
+  }
+
+  renderLearn() {
+    
+  }
+
+  renderEditor() {
+    return <CommandLine />;
   }
 
   render() {
     return (
-        <div className='col-md-5'>
-          <CommandLine />
+      <div className='console col-md-5'>
+        <div className='console-header'>
+          <div className='btn-group' role='group'>
+            <button type='button' className='btn btn-default' onClick={this.renderLearn}><i className='fa fa-book' aria-hidden='true'></i> Learn</button>
+            <button type='button' className='btn btn-default'><i className='fa fa-check-square-o' aria-hidden='true'></i> Instruction</button>
+            <button type='button' className='btn btn-default'><i className='fa fa-code' aria-hidden='true'></i> Code</button>
+            <button type='button' className='btn btn-default'><i className='fa fa-bug' aria-hidden='true'></i> Bug Report</button>
+          </div>
         </div>
+        <div className='console-content'>
+          
+        </div>
+        <div className='console-footer'>
+        </div>
+      </div>
     )
   }
 }
+
+// REMOVED COMMANDLINE
+// render() {
+//   return (
+//       <div className='col-md-5'>
+//         <CommandLine />
+//       </div>
+//   )
+// } 
 
 var mapStateToProps = state => {
   return {
