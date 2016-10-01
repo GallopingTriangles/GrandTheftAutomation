69b5d66307cb4d2a1afe73a2888821f0375b9455
Chris Chiang <cchiang1992@gmail.com>
2016-10-01 10:56:12 -0700

    Create reducer to handle changing user, which will be used for logging in and out

---
+++ src/client/app/reducers/user.js
@@ -0,0 +1,10 @@
+const user = (state = '', action) {
+  switch (action.type) {
+    case 'CHANGE_USER':
+      return action.user;
+    default:
+      return state;
+  }
+};
+
+export default user;
