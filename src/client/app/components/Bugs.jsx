85fa125812a1444d7b0b60b51f814cdc0de1b948
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-10-01 15:53:21 -0700

    (feat) created bugs componenet

---
+++ src/client/app/components/Bugs.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Bugs extends Component {
+	render() {
+		return (
+      <div>
+      	<i className='fa fa-bug' aria-hidden='true'></i>
+      </div>
+		);
+	}
+}
