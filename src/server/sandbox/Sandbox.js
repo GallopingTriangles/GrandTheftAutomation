307de676494a1a128ebab3dbe7c168c028eb447a
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-10-06 13:39:15 -0700

    (feat) changed name to Sandbox

---
+++ src/server/sandbox/Sandbox.js
@@ -0,0 +1,11 @@
+var util = require('util');
+var vm = require('vm');
+// == TEST FRAMEWORK == 
+var runTestSuite = require('./TestingFramework');
+
+// == TESTING SUITE ==
+runTestSuite(function UserInputTest(t) {
+
+
+
+});
