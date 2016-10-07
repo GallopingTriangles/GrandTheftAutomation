1462656b7dcb12f158295083a49af8b3fa02c527
Alex Pattison <alexlpattison@gmail.com>
2016-10-06 20:34:51 -0700

    Move state back to LevelList in preparation for storing highest level in the store

--- src/client/app/components/LevelList.jsx
+++ src/client/app/components/LevelList.jsx
@@ -1,21 +1,43 @@
 import React, { Component } from 'react';
 import LevelListEntry from '../components/LevelListEntry.jsx';
 
-const LevelList = (props) => (
-  <div  className='col-xs-12 col-md-8'>
-    <table>
-      <tbody className="table-hover">
-        <tr>
-        <th className="text-left">Level</th>
-        <th className="text-left">Best Time</th>
-        <th className="text-left">Times wasted</th>
-        </tr>
-        {props.levelData.map(function(levelDatum, index) {
-          return <LevelListEntry levelDatum={levelDatum} key={index}/>
-        })}
-      </tbody>
-    </table>
-  </div>
-)
+var levelData = [
+  {level: 1, time: '00:00', wasted: 30},
+  {level: 2, time: '00:00', wasted: 30},
+  {level: 3, time: '00:00', wasted: 30},
+  {level: 4, time: '00:00', wasted: 30},
+  {level: 5, time: '00:00', wasted: 30},
+  {level: 6, time: '00:00', wasted: 30},
+  {level: 7, time: '00:00', wasted: 30}
+]
+
+class LevelList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentWillMount() {
+    
+  }
+
+  render() {
+    return (
+      <div  className='col-xs-12 col-md-8'>
+        <table>
+          <tbody className="table-hover">
+            <tr>
+            <th className="text-left">Level</th>
+            <th className="text-left">Best Time</th>
+            <th className="text-left">Times wasted</th>
+            </tr>
+            {levelData.map(function(levelDatum, index) {
+              return <LevelListEntry levelDatum={levelDatum} key={index}/>
+            })}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+}
 
 export default LevelList;
--- src/client/app/containers/ProfilePage.jsx
+++ src/client/app/containers/ProfilePage.jsx
@@ -2,16 +2,6 @@ import React, { Component } from 'react';
 import LevelList from '../components/LevelList.jsx';
 import Avatar from '../components/Avatar.jsx';
 
-var levelData = [
-  {level: 1, time: '00:00', wasted: 30},
-  {level: 2, time: '00:00', wasted: 30},
-  {level: 3, time: '00:00', wasted: 30},
-  {level: 4, time: '00:00', wasted: 30},
-  {level: 5, time: '00:00', wasted: 30},
-  {level: 6, time: '00:00', wasted: 30},
-  {level: 7, time: '00:00', wasted: 30}
-]
-
 class ProfilePage extends Component {
   constructor(props) {
     super(props);
@@ -27,7 +17,7 @@ class ProfilePage extends Component {
       <div className='container white-text'>
         <div className='row'>
           <Avatar />
-          <LevelList levelData={levelData} />
+          <LevelList />
         </div>
       </div>
     )
