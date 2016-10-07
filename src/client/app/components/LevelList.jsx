87c3dfc42b4fead7dfe120638d7e92ccb46b5781
Alex Pattison <alexlpattison@gmail.com>
2016-10-06 20:14:26 -0700

    Move hardcoded level data to top level component

--- src/client/app/components/LevelList.jsx
+++ src/client/app/components/LevelList.jsx
@@ -1,17 +1,6 @@
 import React, { Component } from 'react';
 import LevelListEntry from '../components/LevelListEntry.jsx';
 
-var nums = [
-  {level: 1, time: '00:00', wasted: 30},
-  {level: 2, time: '00:00', wasted: 30},
-  {level: 3, time: '00:00', wasted: 30},
-  {level: 4, time: '00:00', wasted: 30},
-  {level: 5, time: '00:00', wasted: 30},
-  {level: 6, time: '00:00', wasted: 30},
-  {level: 7, time: '00:00', wasted: 30}
-
-]
-
 const LevelList = (props) => (
   <div  className='col-xs-12 col-md-8'>
     <table>
@@ -21,8 +10,8 @@ const LevelList = (props) => (
         <th className="text-left">Best Time</th>
         <th className="text-left">Times wasted</th>
         </tr>
-        {nums.map(function(levelData, index) {
-          return <LevelListEntry levelData={levelData} key={index}/>
+        {props.levelData.map(function(levelDatum, index) {
+          return <LevelListEntry levelDatum={levelDatum} key={index}/>
         })}
       </tbody>
     </table>
--- src/client/app/components/LevelListEntry.jsx
+++ src/client/app/components/LevelListEntry.jsx
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 
 const LevelListEntry = (props) => (
     <tr>
-      <td className="text-left">{props.levelData.level}</td>
-      <td className="text-left">{props.levelData.time}</td>
-      <td className="text-left">{props.levelData.wasted}</td>
+      <td className="text-left">{props.levelDatum.level}</td>
+      <td className="text-left">{props.levelDatum.time}</td>
+      <td className="text-left">{props.levelDatum.wasted}</td>
     </tr>
 )
 
