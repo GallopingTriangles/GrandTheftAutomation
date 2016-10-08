410e8d5b644e5d998c0bf075e20ba286f360a423
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-10-08 15:19:19 -0700

    (feat) level 1 test setup

--- src/server/sandbox/levels/level1.js
+++ src/server/sandbox/levels/level1.js
@@ -1,5 +1,59 @@
+var vm = require('vm');
+
+// == USE TESTING FRAMEWORK ===============================
+var runTestSuite = require('../TestingFramework');
+
 var level1 = function(req, res, next) {
-  console.log('IN LEVEL 1');
+  
+  // == TESTING USER INPUT ================================
+  runTestSuite(function UserInputTest(t) {
+  	// USER INPUT
+  	var userInput = req.body.log;
+    // == VIRTUAL MACHINE =================================
+    var funcColor = 'var setColor = function(input) { testColor = input; };';
+    var funcSpeed = 'var setSpeed = function(input) { testSpeed = input; };';
+    var funcEnable = 'var enable = function(input) { testEnable.inputs.push(input); if (input === "engine") { testEngine = true; }; if (input === "sensor") { testSensor = true; }; };';
+
+    // input for virtual machine
+    var input = funcColor + funcSpeed + funcEnable + userInput;
+    var script = new vm.Script(input);
+
+    // sandbox used in virtual machine
+    var sandbox = {
+    	testEnable: {
+    		inputs: []
+    	},
+    	testEngine: undefined,
+    	testColor: undefined,
+    	testSpeed: undefined,
+    	testSensor: undefined,
+    };
+
+    var context = new vm.createContext(sandbox);
+    script.runInContext(context);
+
+    console.log(context);
+
+    // == ENGINE TESTS == //
+    runTestSuite(function EngineInputTest(t) {
+
+    });
+
+    // == COLOR TESTS == //
+    runTestSuite(function ColorInputTest(t) {
+
+    });
+
+    // == SPEED TESTS == //
+    runTestSuite(function SpeedInputTest(t) {
+
+    });
+
+    // == SENSOR TESTS == //
+    runTestSuite(function SensorInputTest(t) {
+
+    });
+  });
 
   next();
 };
