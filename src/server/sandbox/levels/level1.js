d06d4cfd9733dcf4d0dc842a94e0c865c439c271
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-10-09 13:50:39 -0700

    (feat) created path test path for level 4

--- src/server/sandbox/levels/level1.js
+++ src/server/sandbox/levels/level1.js
@@ -239,7 +239,7 @@ var level1 = function(req, res, next) {
 
   // if user level is greater than level 1, run tests of next level
   // and if case is success
-  if (req.body.level === 2 && req.body.phaser.case === 1) {
+  if ((req.body.level === 2 || req.body.level === 4) && req.body.phaser.case === 1) {
     level2(req, res, next);
   } else if (req.body.level === 3 && req.body.phaser.case === 1) {
     level3(req, res, next);
