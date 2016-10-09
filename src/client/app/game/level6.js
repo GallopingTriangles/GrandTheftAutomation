f11f7a75b7201a48206c232bb5b57755dc3a5ff0
Chris Chiang <cchiang1992@gmail.com>
2016-10-09 16:14:43 -0700

    Heavily reduced comments for level6.js

--- src/client/app/game/level6.js
+++ src/client/app/game/level6.js
@@ -20,7 +20,6 @@ var createGame = (userInput) => {
   /**********************************************************/
   /**********************************************************/
 
-  // change width depends on window width, no dynamically resizing yet
   var width = window.innerWidth;
   var height = window.innerHeight;
   var gameWidth = width * (7 / 12) - 10;
@@ -28,10 +27,6 @@ var createGame = (userInput) => {
 
   var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser_game', { preload: preload, create: create, update: update, render: render });
 
-  /*
-  ** The preload() function runs before the game initially renders.
-  ** It loads up all the necessary assets the game needs while running
-  */
   function preload() {
     setCarColor();
     game.load.image('wasted', './assets/wasted.png');
@@ -39,32 +34,21 @@ var createGame = (userInput) => {
     game.load.image('grass', './assets/grass.jpg');
     game.load.image('sensor', './assets/round.png');
 
-    /*
-    ** A spritesheet contains a bunch of frames stitched together to create an animation effect
-    */
     game.load.spritesheet('explosion', './assets/explosion.png', 256, 256, 48);
 
-    /*
-    ** Tilemap is the json file that contains the tile IDs of every tile in each map layer.
-    ** It sets up the map. The tile IDs correspond to the tile in a loaded image through addTilesetImage()
-    */
     game.load.tilemap('level_6', './assets/gameMaps/level_6.json', null, Phaser.Tilemap.TILED_JSON);
     game.load.image('GTA_tileset', './assets/gameMaps/GTA_tileset.png');
   }
 
   var car;
-  // var obstacles;
   var cursors;
   var text;
-
-  /* needsChange */
 
   var sensors = {};
   sensors.left = 'hello';
   sensors.right = 'hello';
   sensors.front = 'hello';
   sensors.back = 'hello';
-
 
   var startingX = 40;
   var startingY = 470;
@@ -83,16 +67,8 @@ var createGame = (userInput) => {
   var carCollisionGroup;
   var obstacleCollisionGroup;
 
-  /*
-  ** An array of collision bodies that the car can collide with.
-  ** A callback will be invoked upon collisions.
-  */
   var collisionBodies;
 
-  /*
-  ** An array of endZone bodies that will trigger a success callback
-  ** when the car hits them, and the level is considered completed.
-  */
   var endZoneBodies;
 
   var completionTiles;
@@ -100,11 +76,6 @@ var createGame = (userInput) => {
   var intersectionTiles_1;
   var coord_1; // the (x,y) coordinate of the center of the intersectionTiles_1
 
-  /*
-  ** The layers that correspond to the tile layers exported in the JSON tilemap file.
-  ** These will be set in the create() function.
-  ** layer_1 contains the tiles to be set in collisionBodies that are collideable with the player
-  */
   var layer_1;
   var layer_2;
   var layer_3;
@@ -113,37 +84,14 @@ var createGame = (userInput) => {
   var layer_6;
   var layer_7;
 
-  /*
-  ** The create() function is called automatically after preload() has finished.
-  ** Sprite, particles, and most everything else can be created here that has access to
-  ** the assets initialized from the preload(). The create() function contains the bulk
-  ** of the set-up code, such as creating game objects.
-  */
   function create() {
-    /*
-    ** Set the initial state and physics engine for the game.
-    ** Enable impact event handling before any impact events are dispatched.
-    */
+
     game.physics.startSystem(Phaser.Physics.P2JS);
     game.physics.p2.setImpactEvents(true);
 
-    /*
-    ** Set the tilemap for the game, which creates a grid system.
-    ** Tiles can be added on top in different layers,
-    ** and collisions can be specific for certain tiles in certain layers.
-    ** http://phaser.io/docs/2.6.2/Phaser.Tilemap.html#addTilesetImage
-    */
     map = game.add.tilemap('level_6');
     map.addTilesetImage('GTA_tileset');
 
-    // map = game.add.tilemap('map');
-    // map.addTilesetImage('tmw_desert_spacing');
-
-    /*
-    ** Set the layers and their respective tile IDs for collision.
-    ** Needs to be done before generating the p2 bodies below.
-    ** The layer names must correspond to the layers from the JSON tilemap file
-    */
     layer_2 = map.createLayer('road_layer');
     layer_3 = map.createLayer('building_layer');
     layer_4 = map.createLayer('street_stuff_layer');
@@ -152,36 +100,10 @@ var createGame = (userInput) => {
     layer_7 = map.createLayer('intersection_DR_layer');
     layer_1 = map.createLayer('collision_layer');
 
-
-    /*
-    ** Set the appropriate tiles of a certain layer to be collideable
-    ** http://phaser.io/docs/2.6.2/Phaser.Tilemap.html#setCollision
-    */
     map.setCollisionBetween(0, 2000, true, 'collision_layer');
 
-    // map.setCollisionBetween(0, 2000, true, 'end_zone_layer');
-
-    /*
-    ** Convert the collision-enabled tile layer into Phaser p2 bodies. Only tiles
-    ** that collide are created. This returns an array of body objects that can be
-    ** controlled by additional actions.
-    ** http://phaser.io/docs/2.6.2/Phaser.Physics.P2.html#convertTilemap
-    */
     collisionBodies = game.physics.p2.convertTilemap(map, layer_1, true, false);
 
-    /*
-    ** Convert the endZoneBodies into Phaser p2 bodies so the game can detect when
-    ** the car has entered any of these tiles, which will be interpreted as a level completion.
-    */
-    // endZoneBodies = game.physics.p2.convertTilemap(map, layer_5, true, false);
-    // console.log(endZoneBodies);
-
-
-
-    /*
-    ** Gather the endzone tiles and intersection tiles so a callback can be run when
-    ** the car reaches any of these tiles, such as a turn or completion zone.
-    */
     completionTiles = layer_5.getTiles(0, 0, 2000, 2000).filter(function(tile) {
       return tile.index > 0;
     });
@@ -190,81 +112,34 @@ var createGame = (userInput) => {
       return tile.index > 0;
     })
 
-    /*
-    ** Gather all tiles from layer_1 into an array of tiles,
-    ** and assign a callback function to when these tiles are hit by anything.
-    */
-    // collisionTiles = layer_1.getTiles(0, 0, 800, 600).filter(function(tile) {
-    //   return tile.index > 0;
-    // });
-
-
-    /*
-    ** Initiates the car sensor, the car body, and sets the speed based on the user input
-    */
     if (FAKE_USER_INPUT.sensor) {
       createSensors();
     }
     createCar();
     setSpeed();
 
-    /*
-    ** Create two collision groups. One for the car and one for everything else.
-    ** A collision will be detected for items in collision groups, which will invoke a callback.
-    ** Update Bounds Collision Group will allow objects to collide with the world bounds.
-    */
     carCollisionGroup = game.physics.p2.createCollisionGroup();
     obstacleCollisionGroup = game.physics.p2.createCollisionGroup();
     game.physics.p2.updateBoundsCollisionGroup();
     car.body.setCollisionGroup(carCollisionGroup);
 
-    /*
-    ** Assign each tile from the collisionBodies into the obstacleCollisionGroup.
-    ** These tiles will be set to collide with other tile bodies and the car, since
-    ** the car is set to collide with objects in the obstacleCollisionGroup.
-    ** http://phaser.io/docs/2.6.2/Phaser.Physics.P2.Body.html#setCollisionGroup
-    */
     collisionBodies.forEach(function(collisionBody) {
       collisionBody.setCollisionGroup(obstacleCollisionGroup);
       collisionBody.collides([carCollisionGroup, obstacleCollisionGroup]);
-      // collisionBody.debug = true;
-
     })
 
-    /*
-    ** The gameOver callback is called when a collision is detected
-    ** between the car and any body in the obstacleCollisionGroup (the tiles).
-    */
     car.body.collides(obstacleCollisionGroup, gameOver, this);
 
-
-    /*
-    ** Enables the user to have control over the car through their cursor keys
-    */
     cursors = game.input.keyboard.createCursorKeys();
     coord_1 = intersectionCenter(intersectionTiles_1); // pixel center of the first intersection
-
   }
 
-
-
-
-
   function update() {
-    /*
-    ** Enable sensor functionality if the user has activated the car sensor.
-    ** If the sensor detects any overlapping collision bodies, it will turn on.
-    */
+
     if (userInput.sensor) {
 
       attachSensors(0, 100, sensors);
 
-      /*
-      ** In every frame of the game, examine every collision body (tile) and check if
-      ** any of its corners are inside the sensor area. This serves as a listener to
-      ** detect overlapping between a sensor and collision bodies. If an overlap is
-      ** detected, set the variable overlap to true.
-      */
       var overlap = false;
       collisionBodies.forEach(function(body) {
         for (var sensor in sensors) {
@@ -277,9 +152,6 @@ var createGame = (userInput) => {
         }
       })
 
-      /*
-      ** Increase the opacity of the sensor while a collision body is in its area.
-      */
       // if (overlap) {
       //   sensor.alpha = 0.7;
       // } else {
@@ -288,19 +160,13 @@ var createGame = (userInput) => {
     }
 
     if (FAKE_USER_INPUT.case === 1) {
-      
+
+      checkCompletion();
     }
 
-    /*
-    ** The car should remain still if no arrow keys are pressed for early levels.
-    ** This resets the car's velocity per frame.
-    */
-    checkCompletion();
   }
 
   function render() {
-    // game.debug.spriteInfo(car, 32, 32);
-    // car.body.debug = true;
 
   }
 
@@ -309,18 +175,11 @@ var createGame = (userInput) => {
   /*************** HELPER FUNCTIONS **************/
   /******************* HELPER FUNCTIONS **********/
 
-  /*
-  ** Generates the car as a Phaser sprite object. Enable it to be a Phaser body object.
-  ** Sets a rectangle to the size of the car to interpret collisions.
-  ** Initialize the starting coordinates to match up with the sensor's coordinates.
-  */
   function createCar() {
-    // Appearance
     car = game.add.sprite(startingX, startingY, 'car');
     car.anchor.setTo(.3, .5);
     car.scale.setTo(carScale);
 
-    // Physics
     game.physics.p2.enable(car);
     car.body.setRectangle(car.width, car.height);
     car.body.collideWorldBounds = true;
@@ -330,9 +189,6 @@ var createGame = (userInput) => {
     }
   }
 
-  /*
-  ** Determine which image to load as the car sprite, based on user input.
-  */
   function setCarColor() {
     switch(userInput.color) {
       case 'white':
@@ -355,10 +211,6 @@ var createGame = (userInput) => {
     }
   }
 
-  /*
-  ** Dictates which direction the car should rotate based on if the car
-  ** is moving in a forward or reverse direction.
-  */
   function leftRight(forward) {
     var angularVelocity;
 
@@ -384,12 +236,9 @@ var createGame = (userInput) => {
   }
 
 
-  /* needsChange */
 
   function createSensors() {
-    // Check to make sure the user has turned the sensor on
     if (userInput.sensor) {
-      // Appearace
       for (var sensor in sensors) {
         sensors[sensor] = game.add.sprite(startingX, startingY, 'sensor')
         sensors[sensor].alpha = .1;
@@ -409,10 +258,6 @@ var createGame = (userInput) => {
     }
   }
 
-  /*
-  ** If the user has declared a speed:
-  ** Set the car's speed appropriately.
-  */
   function setSpeed() {
     if (userInput.speed) {
       carForwardSpeed = userInput.speed * userSpeedMultiplier;
@@ -420,10 +265,6 @@ var createGame = (userInput) => {
     }
   }
 
-  /*
-  ** Checks if the bounds of the car ever overlap with the tiles of the
-  ** completion layer. If so, pause the game and render the level completion.
-  */
   function checkCompletion() {
     completionTiles.forEach(function(tile) {
       if (Math.abs(tile.worldX + 16 - car.body.x) < 25 && Math.abs(tile.worldY +16 - car.body.y) < 25) {
@@ -439,13 +280,6 @@ var createGame = (userInput) => {
     console.log('COMPLETED!');
   }
 
-  /*
-  ** Called when the car collides with a collision body (tile)
-  ** The car gloriously bursts into flames while the classic WASTED
-  ** text renders onto the screen. Pure awesomeness.
-  ** How it's done: remove the car from the game, and add the explosion
-  ** spritesheet. Animate it by going through the frames of the spritesheet.
-  */
   function gameOver() {
     explosion = game.add.sprite(400, 300, 'explosion');
     explosion.x = car.x;
@@ -475,10 +309,6 @@ var createGame = (userInput) => {
 
     return [x, y];
   }
-
-
-
-
 
 }
 
