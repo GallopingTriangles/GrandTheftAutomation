5fb9eab70b7263b1f2e5e7f9587a32c4439f6cc3
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-10-09 16:44:05 -0700

    (feat) enabled tests to check if route is enabled

--- src/server/sandbox/levels/level5.js
+++ src/server/sandbox/levels/level5.js
@@ -35,13 +35,33 @@ var level5 = function(req, res, next) {
   // == VIRTUAL MACHINE =================================
   var funcColor = 'var setColor = function(input) { testColor = input; };';
   var funcSpeed = 'var setSpeed = function(input) { testSpeed = input; };';
-  var funcEnable = 'var enable = function(input) { testEnable.push(input); if (input === "engine") { testEngine = true; }; if (input === "sensor") { testSensor = true; }; };';
+  var funcEnable = 'var enable = function(input) { testEnable.push(input); if (input === "engine") { testEngine = true; }; if (input === "sensor") { testSensor = true; }; if (input === "route") { testRoute = true }; };';
   var funcTurn = 'var turn = function(input) { testTurn.value = input; testTurn.count++ };';
-  var funcRoute = 'var route = function(input) { testRoute = input; };';
+  var funcRoute = 'var route = function(input) { directions = input; };';
 
   // input for virtual machine
   var input = funcColor + funcSpeed + funcEnable + funcTurn + funcRoute + userInput;
   var script = new vm.Script(input);
+
+  var sandbox = {
+  	sensor: {
+  		front: false
+  	},
+  	map: {
+      intersection: false
+  	},
+  	directions: undefined,
+  	testEnable: [],
+  	testEngine: undefined,
+  	testColor: undefined,
+  	testSpeed: undefined,
+  	testSensor: undefined,
+  	testRoute: undefined,
+  	testRoute: undefined
+  };
+
+  var context = new vm.createContext(sandbox);
+  script.runInContext(context);
 
   var setCaseCount = 1;
   var setCase = function(caseNo) {
@@ -51,14 +71,50 @@ var level5 = function(req, res, next) {
   	}
   };
 
+  console.log('CONTEXT', context);
+
   // == ENABLED TESTS == //
   runTestSuite(function EnabledInputTest(t) {
+  	// enabled array in sandbox context
+  	var enabled = context.testEnable;
+  	// test if enabled function has expected behaviour
+    this.testEnabledCalledThreeTimes = function() {
+      t.assertTrue(
+        enabled.length === 3,
+        'Expect enable() to be called three times, but got called ' + enabled.length + ' times',
+        function() {
+        	setCase(2); // error, don't let car move
+        }
+      );
+    };
 
+    // test if all the input is of data type string
+    this.testEnableInputType = function() {
+    	t.assertOptionsOfTypeString(
+        enabled,
+        function() {
+        	setCase(4); // input error of lastly added item, car crashes
+        }
+    	);
+    };
+
+    // test if route is enabled thirdly
+    this.testRouteEnabledThirdly = function() {
+      var enabledThird = '';
+      context.testEnable[2] ? enabledThird = context.testEnable[2] : enabledThird = '';
+      t.assertTrue(
+        enabledThird === 'route',
+        'Expected route to be enabled thirdly, but got ' + enabledThird + ' enabled thirdly',
+        function() {
+          setCase(4); // syntax error, route not enabled. car crashes
+        }
+      );
+    }
   });
 
   // == ROUTE TESTS == //
   runTestSuite(function RouteInputTest(t) {
-
+    
   });
 
 	});
