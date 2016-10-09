9403f91d7c162fdf8706447605801b1d7b31c912
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-10-09 15:44:34 -0700

    (feat) created file for level 5

---
+++ src/server/sandbox/levels/level5.js
@@ -0,0 +1,24 @@
+var vm = require('vm');
+
+// == USE TESTING FRAMEWORK ===============================
+var runTestSuite = require('../TestingFramework');
+
+var level5 = function(req, res, next) {
+
+	// == EXPECTED USER INPUT ===============================
+	// 
+	// enable('engine');
+	//
+	// setColor('white / black / red / blue');
+  // 
+  // setSpeed(100);
+  //
+  // enable('sensor');
+  //
+  // enable('route');
+  // 
+  // route(['left']) |or| route(['right'])
+  //
+	// ======================================================
+
+}
