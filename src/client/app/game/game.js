5ddf1f86924bc4d15c62bce491e640f608763626
Chris Chiang <cchiang1992@gmail.com>
2016-10-09 17:02:55 -0700

    comments in game

--- src/client/app/game/game.js
+++ src/client/app/game/game.js
@@ -8,25 +8,30 @@ var level7 = require('./level7.js');
 
 export default function(userInput, level) {
   // create the game for the appropriate level
-  // if (level === 1) {
-  //   level1(userInput);
-  // } else if (level === 2) {
-  //   level2(userInput);
-  // } else if (level === 3) {
-  //   level3(userInput);
-  // } else if (level === 4) {
-  //   level4(userInput);
-  // } else if (level === 5) {
-  //   level5(userInput);
-  // }
+  if (level === 1) {
+    level1(userInput);
+  } else if (level === 2) {
+    level2(userInput);
+  } else if (level === 3) {
+    level3(userInput);
+  } else if (level === 4) {
+    level4(userInput);
+  } else if (level === 5) {
+    level5(userInput);
+  } else if (level === 6) {
+    level6(userInput);
+  } else if (level === 7) {
+    level7(userInput);
+  }
 
+  // hardcoding which level to render for troubleshooting
   // level1(userInput);
   // level2(userInput);
   // level3(userInput);
   // level4(userInput);
   // level5(userInput);
   // level6(userInput);
-  level7(userInput);
+  // level7(userInput);
 
 }
 //
