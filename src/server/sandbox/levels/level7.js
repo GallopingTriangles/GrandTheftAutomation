99a160e43981980515db46f0b811de4e237640f6
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-10-10 16:40:05 -0700

    (feat) comments and cases level 7

--- src/server/sandbox/levels/level7.js
+++ src/server/sandbox/levels/level7.js
@@ -1,9 +1,31 @@
-// case: 2, // fail, didn't enable the engine
-    // case: 3, // fail, drove STRAIGHT through the FIRST intersection and crashed
-    // case: 4, // fail, turned LEFT at FIRST intersection but drove STRAIGHT through the SECOND intersection and crashed
-    // case: 5, // fail, turned RIGHT at FIRST intersection and crashed
-    // case: 6, // fail, turned LEFT at FIRST intersection but turned RIGHT at SECOND intersection and crashed
 var vm = require('vm');
+
+// == USE TESTING FRAMEWORK ===============================
+var runTestSuite = require('../TestingFramework');
+
+	// == EXPECTED USER INPUT ===============================
+	// 
+	// enable('engine');
+	//
+	// setColor('white / black / red / blue');
+  // 
+  // setSpeed(100);
+  //
+  // enable('sensor');
+  //
+  // enable('route');
+  // 
+  // route(['left', 'left']);
+  //
+	// ======================================================
+
+	// == CASES =============================================
+	// case 1: success, left turn followed by left turn
+	// case 2: fail, invalid engine or speed
+	// case 3: fail, drove straight at first intersection
+	// case 4: fail, turned left at first intersection but drove straight at second intersection
+	// case 5: fail, turned right at first intersection
+	// case 6: fail, turned left at first intersection and turned right at second intersection
 
 var level7 = function(req, res, next) {
 
