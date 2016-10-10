3a2fcbbb2f6baa41bfa6b1fb656c9f36d71fbc0e
Chris Chiang <cchiang1992@gmail.com>
2016-10-10 12:25:11 -0700

    Add textfile for instructions

---
+++ src/client/app/components/instructionsTextFile.js
@@ -0,0 +1,17 @@
+module.exports = {
+  
+  one:            'LEVEL 1\n' +
+                  'Welcome to GTA\n' +
+                  '(a sentence for introducing AV shit...)\n' +
+                  'Let\'s turn on your engine, need to use a special tool called "enable"\n' +
+                  'Type <code> enable(\'engine\'); </code>\n' +
+                  'Now let\'s give the car a speed\n' +
+                  'Type <code> setSpeed(30) </code>\n' +
+                  'If you want to customize your car... <code> setColor(\'black\'); </code>' +
+                  'The semicolons are used to separate statements';
+                  'Click RUN to see the car you\'ve just programmed!',
+
+  two: '',
+  three: '',
+  four: '',
+}
