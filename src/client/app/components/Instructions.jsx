06f69418195cade6418cc10741ba8f5493990c91
Chris Chiang <cchiang1992@gmail.com>
2016-10-10 12:22:30 -0700

    level one instructions

--- src/client/app/components/Instructions.jsx
+++ src/client/app/components/Instructions.jsx
@@ -16,3 +16,14 @@ export default class Instructions extends Component {
 		);
 	}
 }
+
+var levelOne = 'LEVEL 1\n' +
+          'Welcome to GTA\n' +
+          '(a sentence for introducing AV shit...)\n' +
+          'Let\'s turn on your engine, need to use a special tool called "enable"\n' +
+          'Type <code> enable(\'engine\'); </code>\n' +
+          'Now let\'s give the car a speed\n' +
+          'Type <code> setSpeed(30) </code>\n' +
+          'If you want to customize your car... <code> setColor(\'black\'); </code>' +
+          'The semicolons are used to separate statements';
+          'Click RUN to see the car you\'ve just programmed!';
