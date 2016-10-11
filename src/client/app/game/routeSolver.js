5248a3053d60a4bd1d6436f2f997d01caa9887c7
Chris Chiang <cchiang1992@gmail.com>
2016-10-10 17:34:17 -0700

    Add routeSolver file

--- src/client/app/game/routeSolver.js
+++ src/client/app/game/routeSolver.js
@@ -1,1 +1,20 @@
-module.exports = function(){};
+module.exports = function(list, startDirection, startNode, endNode, endDirection) {
+
+};
+
+/*
+[
+  [0,0,0,0,0],
+  [0,0,0,0,0],
+  [0,0,0,0,0],
+  [0,0,0,0,0],
+]
+
+
+[
+  [1,0,1,0,0],
+  [1,0,1,0,0],
+  [0,0,0,0,0],
+  [0,0,0,0,0],
+]
+*/
