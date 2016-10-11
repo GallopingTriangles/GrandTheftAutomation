3ec0e0a8539703939883bcd96ec2a185197e5b8f
Kevin Chan <kevindchan@gmail.com>
2016-10-11 14:43:19 -0700

    Adds comments to all middleware, indluding session configuration (lines 24-47)

--- src/server/server.js
+++ src/server/server.js
@@ -15,32 +15,34 @@ var express = require('express'),
 
 var app = express();
 
-// == serve static assets ===================================
+// == serve static assets =====================================
 // serve static client-side files
 app.use(express.static(path.join(__dirname, '../../dist')));    
 // serve node_modules from post-compiled /lib file
 app.use('/lib', (express.static(path.join(__dirname, '../../node_modules'))));  
 
-// == middleware ============================================
-// app.use(cookieParser(config.secret));
+// == middleware ==============================================
 app.use(bodyParser.urlencoded({ extended: true }));             // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());                                     // parse application/json
 app.use(morgan('dev'));                                         // log every request to the console
-// == session ===============================================
+
+// == mysql session store =====================================
 var sessionStore = new MySQLStore({                             // initializes a mysql session store
-  host: 'localhost',
-  port: 3306,
-  user: config.dbUsername,
-  password: config.dbPassword,
-  database: config.dbName
-}); // WHEN TO CLOSE SESSION STORE? => sessionStore.close();
+  host: 'localhost',                                            // this property will change if/when app is deployed
+  port: 3306,                                                   // this property will change if/when app is deployed
+  user: config.dbUsername,                                      // mysql username
+  password: config.dbPassword,                                  // mysql password for username above
+  database: config.dbName                                       // mysql database name for project
+});
+
+// == express session ========================================= 
 app.use(session({                                               // configures express session
-  secret: config.secret,
-  store: sessionStore,
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    maxAge: 7200000
+  secret: config.secret,                                        // requires session secret to secure
+  store: sessionStore,                                          // all sessions to be store in mysql session store (above)
+  resave: false,                                                // will not force save sessions to store when sessions are not modified
+  saveUninitialized: false,                                     // will not save session to store when new but not modified
+  cookie: {                                                     // cookie options
+    maxAge: 7200000                                             // sets expiration date of cookie in milliseconds beyond current server time
   }
 }));
 // == passport/authentication ===============================================
