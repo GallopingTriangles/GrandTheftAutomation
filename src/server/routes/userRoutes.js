033347fd8b3cf3720b057c6d892c48c9683f7846
Kevin Chan <kevindchan@gmail.com>
2016-10-11 15:32:35 -0700

    Adds comments to userRoutes.js

--- src/server/routes/userRoutes.js
+++ src/server/routes/userRoutes.js
@@ -1,16 +1,24 @@
 var router = require('express').Router();
 var userController = require('../controllers/userController.js');
 
-router.route('/')
-  .get(userController.getState); // returns user's saved stuff after verification
+/****************** ROUTER FOR '/users' ENDPOINT ***********************
+** Within the '/users' endpoint are several routes associated to user ** 
+** profiles (login, sign up, and logout). Each route accommodates     **
+** specific HTTP request types, where corresponding controllers from  **
+** userController.js are supplied to handle certain actions. Refer to **
+** userController.js for more information.                            **
+************************************************************************/
 
-router.route('/login')
-  .post(userController.login); // authenticates the user when loggin in
+router.route('/')                 // handles routes with '/users/' endpoint
+  .get(userController.getState);  // returns user's saved stuff after verification
 
-router.route('/signup')
-  .post(userController.signup); // checks and creates a new user to the database
+router.route('/login')            // handles routes with '/users/login' endpoint
+  .post(userController.login);    // logs user in and creates new session
 
-router.route('/logout')
-  .get(userController.logout); // wipes the session and logs the user out
+router.route('/signup')           // handles routes with '/users/signup' endpoint
+  .post(userController.signup);   // creates a new user by saving to the database
 
-module.exports = router;
+router.route('/logout')           // handles routes with '/users/logout' endpoint
+  .get(userController.logout);    // logs out user by destroying session on database
+
+module.exports = router;          // exports all routes
