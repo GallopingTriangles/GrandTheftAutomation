996cd18f8e9c1fda542dad75fa8df4737c177216
Kevin Chan <kevindchan@gmail.com>
2016-10-11 15:41:44 -0700

    Refactors route in gameRoutes.js to use router.route instead of router

--- src/server/routes/gameRoutes.js
+++ src/server/routes/gameRoutes.js
@@ -3,11 +3,21 @@ var gameController = require('../controllers/gameController.js');
 var sandbox = require('../middleware/sandbox.js');
 var sandboxRoutes = require('../sandbox/sandboxRoutes.js');
 
-router.get('/', gameController.getGameState); // gets the user's saved game state of the code editor
+/******************* ROUTER FOR '/game' ENDPOINT ***********************
+** This router handles all HTTP requests to '/game/'.  ** 
+**   **
+**   **
+**   **
+**   **
+************************************************************************/
+
+router.route('/')
+  .get(gameController.getGameState) // gets the user's saved game state of the code editor
+  .post(sandboxRoutes, gameController.saveGameState)
 
 // saves the user's game state of the code editor
 // sandbox middleware parses user input and adds it to req.body
-router.post('/', sandboxRoutes, gameController.saveGameState); 
+
 
 // router.put('/', gameController.updateGameState); // updates the user's game state of the code editor
 /****************************************************************************
