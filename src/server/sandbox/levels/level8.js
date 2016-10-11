4bbe8b45414a6f61fd0c9948131591d3f84b28da
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-10-11 11:32:33 -0700

    (feat) created enabled tests

--- src/server/sandbox/levels/level8.js
+++ src/server/sandbox/levels/level8.js
@@ -52,7 +52,7 @@ var level8 = function(req, res, next) {
 	  // == VIRTUAL MACHINE =================================
 	  var funcColor = 'var setColor = function(input) { testColor = input; };';
 	  var funcSpeed = 'var setSpeed = function(input) { testSpeed = input; };';
-	  var funcEnable = 'var enable = function(input) { testEnable.values.push(input); testEnable.count++; if (input === "engine") { testEngine = true; }; if (input === "sensor") { testSensor = true; }; if (input === "gps") { testGps = true }; };';
+	  var funcEnable = 'var enable = function(input) { testEnabled.values.push(input); testEnabled.count++; if (input === "engine") { testEngine = true; }; if (input === "sensor") { testSensor = true; }; if (input === "gps") { testGps = true }; };';
 	  var funcTurn = 'var turn = function(input) { testTurn.value = input; testTurn.count++ };';
 	  var funcRoute = 'var setRoute = function(input) { route.directions = input; route.count++ };';
 
@@ -75,7 +75,7 @@ var level8 = function(req, res, next) {
 		  		directions: undefined,
 		  		count: 0
 		  	},
-		  	testEnable: {
+		  	testEnabled: {
           values: [],
           count: 0
 		  	},
@@ -108,12 +108,53 @@ var level8 = function(req, res, next) {
 	  	// create new virtual machine
 	  	var context = new vm.createContext(sb);
 		  script.runInContext(context);
+      
+      //console.log(context);
 
-      console.log(context);
+      var enabled = context.testEnabled.values;
+      var calls = context.testEnabled.count;
 
       this.testEnabledCalledThreeTimes = function() {
-
+        t.assertTrue(
+          calls === 3,
+          'Expected function enabled() to be called 3 times, but got called ' + calls + ' time(s)',
+          function() {
+          	setCase(4);
+          }
+        );
       };
+
+      this.testEnabledCalledWithArgument = function() {
+        t.assertTrue(
+          enabled[2],
+          'Expected function enabled() to be called with an argument, but got ' + enabled[2],
+          function() {
+            setCase(4);
+          }
+        );
+      };
+
+      this.testEnabledInputTypeString = function() {
+        t.assertTrue(
+          typeof enabled[2] === 'string',
+          'Expected function enabled() to be called with an argument of type string, but got called with type ' + typeof enabled[2],
+          function() {
+          	setCase(4);
+          } 
+        );
+      };
+
+      this.testEnabledInputValueGps = function() {
+        t.assertTrue(
+          enabled[2] === 'gps',
+          'Expected function enabled() to be called with argument "gps", but got called with ' + enabled[2],
+          function() {
+          	setCase(4);
+          }
+        );
+      };
+
+
 	  });
 
     // == CONDITIONAL LEFT TESTS == //
