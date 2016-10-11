73d62282a403b68105c2927cc845f6c0f4292fa8
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-10-11 09:35:59 -0700

    (feat) created file for level 8

---
+++ src/server/sandbox/levels/level8.js
@@ -0,0 +1,10 @@
+var vm = require('vm');
+
+// == USE TESTING FRAMEWORK ===============================
+var runTestSuite = require('../TestingFramework');
+
+var level8 = function(req, res, next) {
+
+};
+
+module.exports = level8;
