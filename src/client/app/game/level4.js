13f2ff2c257c4157e863c4e3750701d6971d8786
Alex Pattison <alexlpattison@gmail.com>
2016-10-12 10:05:35 -0700

    level 4 sensor working

--- src/client/app/game/level4.js
+++ src/client/app/game/level4.js
@@ -331,7 +331,7 @@ var createGame = (userInput) => {
   }
 
   function enableSensors() {
-    
+
     for (var sensor in sensors) {
       sensors[sensor].angle = car.body.angle;
       sensors[sensor].alpha = .3;
@@ -401,16 +401,20 @@ var createGame = (userInput) => {
     explosion.anchor.setTo(.5, .5);
     explosion.animations.add('explode');
     explosion.animations.play('explode', 24, false);
-    // text.kill();
     car.kill();
-    if (sensors.right) {
-      for (var sensor in sensors) {
+    for (var sensor in sensors) {
+      if (sensors[sensor] !== {}) {
         sensors[sensor].kill();
       }
     }
     wasted = game.add.sprite(400, 300, 'wasted');
     wasted.anchor.setTo(.5, .5);
+
+    setTimeout(() => {
+      game.paused = true;
+    }, 3000)
   }
+
 
   function intersectionCenter(tiles) {
     // returns the center (x,y) pixel of an intersection layer
