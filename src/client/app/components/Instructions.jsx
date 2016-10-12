3088277246789d96ae0cbbca90f7bc43fc2031c2
Chris Chiang <cchiang1992@gmail.com>
2016-10-11 17:05:54 -0700

    instructions levels 0-11

--- src/client/app/components/Instructions.jsx
+++ src/client/app/components/Instructions.jsx
@@ -5,6 +5,7 @@ export default class Instructions extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      0: level.zero,
       1: level.one,
       2: level.two,
       3: level.three,
@@ -12,7 +13,10 @@ export default class Instructions extends Component {
       5: level.five,
       6: level.six,
       7: level.seven,
-      8: level.eight
+      8: level.eight,
+      9: level.nine,
+      10: level.ten,
+      11: level.eleven
     };
   }
 	render() {
