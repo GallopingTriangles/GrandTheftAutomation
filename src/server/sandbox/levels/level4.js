4d121f9dd90c27df67c29cbf54ebf1a6492a6aa2
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-10-12 10:02:05 -0700

    (refac) refactored tests to output correct case up to level 4

--- src/server/sandbox/levels/level4.js
+++ src/server/sandbox/levels/level4.js
@@ -1,10 +1,10 @@
 var level4 = function(req, res, next) {
 
-	// case: 2, // fail, crashed into obstacle after right turn
+	// case: 2, // fail, crashed into obstacle after right turn --> CHANGE to car is not moving at all
   // case: 3, // fail, crash straight
   // case: 4, // fail, crash left
   // case: 5, // fail, stalls at intersection
-  // case: 6, // fail, car did not start engine
+  // case: 6, // fail, car did not start engine --> CHANGE to crash into obstacle after right turn
 
   next();
 };
