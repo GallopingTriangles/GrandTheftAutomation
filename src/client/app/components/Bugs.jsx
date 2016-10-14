47d04db272771cb3138b38389bc899c6f9d1c8ee
Chris Chiang <cchiang1992@gmail.com>
2016-10-14 12:09:56 -0700

    Algorithm for wrapping functions in span

--- src/client/app/components/Bugs.jsx
+++ src/client/app/components/Bugs.jsx
@@ -7,10 +7,11 @@ class Bugs extends Component {
       <div className='instructions'>
 
         { this.props.bugs.map(message => {
+          console.log(message);
           return (
             <div>
               <p className='fa fa-bug' aria-hidden='true'></p>
-              <p> { message } </p>
+              <p>{ formatBugs(message) }</p>
             </div>
           )
         })}
@@ -18,6 +19,37 @@ class Bugs extends Component {
       </div>
 		);
 	}
+}
+
+function formatBugs(message) {
+  var words = message.split(' ');
+  var i;
+  words.forEach((word, ind) => {
+    if (word.indexOf('()') > -1) {
+      i = ind;
+    }
+  })
+  if (!i) { // if there is no function in it
+    return message;
+  }
+  
+  var first = '';
+  var last = '';
+  var span = (<span>{ words[i] } </span>);
+  words.forEach((word, ind) => {
+    if (ind < i) {
+      first += word + ' ';
+    } else if (ind > i) {
+      last += word + ' '
+    }
+  })
+  return (
+    <p>
+      <p style={{ display: 'inline '}} >{ first }</p>
+      { span }
+      <p style={{ display: 'inline '}} >{ last }</p>
+    </p>
+  )
 }
 
 export default Bugs;
